ba6e3f5efcc11bad7eba52355751eaa8b3e85135
vineet <mrsingh721767570000@gmail.com>
2024-10-09 22:44:23 +0530

    variables in java.

---
+++ variables.java
@@ -0,0 +1,20 @@
+/**
+ * variables
+ */
+public class variables {
+
+    public static void main(String[] args) {
+        /* int num =9;
+        System.out.println(num); */
+        /* byte num =8+5+3;
+        System.out.println(num); */
+        byte num1 =4;
+        byte num2 =89;
+        byte num3 =9;
+        int sum = num1 + num2 +num3;
+        System.out.print("The sum of these numbers is:");
+        System.out.println(sum);
+
+
+    }
+}
