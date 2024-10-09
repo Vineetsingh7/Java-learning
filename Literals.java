64377a6566a71971698d5859dd73a15b998e0b90
vineet <mrsingh721767570000@gmail.com>
2024-10-09 23:36:27 +0530

    Literals in java.

---
+++ Literals.java
@@ -0,0 +1,24 @@
+/**
+ * Literals
+ */
+public class Literals {
+
+    public static void main(String[] args) {
+           byte age =20;
+           System.out.println(age);
+           char c ='v';
+           System.out.println(c);
+           float f= 4.9f;
+           System.out.println(f);
+           double d=4.69;
+           System.out.println(d);
+           int i= 486;
+           System.out.println(i);
+           long l =4865954569469653L;
+           System.out.println(l);
+           short s =48;
+           System.out.println(s);
+           String str ="Vineet singh";
+           System.out.println(str);
+    }
+}
