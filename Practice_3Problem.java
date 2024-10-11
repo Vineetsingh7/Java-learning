5289884c7840e397622b3f438f4ba8aa3b1032a2
vineet <mrsingh721767570000@gmail.com>
2024-10-11 21:05:41 +0530

    Practice problem in java.

---
+++ Practice_3Problem.java
@@ -0,0 +1,14 @@
+/**
+ * Practice_3Problem
+ */
+public class Practice_3Problem {
+ 
+    public static void main(String[] args) {
+    /* Write a program to calculate CGPA */
+        float sub1 =99 ;
+    float sub2 = 86;
+float sub3 = 33;
+ float Cgpa = (sub1 + sub2 + sub3 )/ 30;
+System.out.println(Cgpa);
+}
+}
