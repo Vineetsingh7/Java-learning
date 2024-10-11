54fcc3af3b20f7b54388e941107f16032724d547
vineet <mrsingh721767570000@gmail.com>
2024-10-11 21:56:56 +0530

    Operators in java.

---
+++ Operators.java
@@ -0,0 +1,19 @@
+/**
+ * Operators
+ */
+public class Operators {
+
+    public static void main(String[] args) {
+        int a =5 ;
+        int b = 89 +a ;
+        System.out.println(b);
+        int c =5 ;
+        int d=  89-c ;
+        System.out.println(d);  
+        int e= b %a ;//Modulo operators.(reminder)
+        System.out.println(e);  
+        System.out.println(8==9);
+        System.out.println(64>5);
+        System.out.println(65>58 && 85>58);
+    }
+    }
