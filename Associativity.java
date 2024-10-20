b1e5ff842376a30e0dfeb135566b8b750a576b23
vineet <mrsingh721767570000@gmail.com>
2024-10-20 23:17:22 +0530

    Precedence and associativity in java.

---
+++ Associativity.java
@@ -0,0 +1,18 @@
+/**
+ * Prece&Associativity
+ */
+public class Associativity {
+
+    public static void main(String[] args) {
+        int a = 60*2-500/10; 
+           /* board mass */
+             /*  =120-50
+                  =70 */
+        System.out.println(a);
+       /*  Precedence */
+       int b = 60/2-34*10;
+          /*  =30-340
+              =-310 */
+              System.out.println(b);
+    }
+}
