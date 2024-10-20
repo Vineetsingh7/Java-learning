d8d75ace7f38394b3715cb4049012b96be9423c6
vineet <mrsingh721767570000@gmail.com>
2024-10-20 23:36:18 +0530

    Quiz on Precedence in java.

---
+++ QuickQuiz.java
@@ -0,0 +1,20 @@
+/**
+ * QuickQuiz
+ */
+public class QuickQuiz {
+
+    public static void main(String[] args) {
+        int x =5;
+        int y = 2;
+        int z = x*y/2;
+        System.out.println(z);
+        int a =5;
+        int b =2;
+        int c =3;
+        int k =b*b-(4*a*c)/(2*a);
+        System.out.println(k);
+
+
+    
+    }
+}
