4670d7bf1b39a7f5520dc5be271ae2d1397e3d1d
vineet <mrsingh721767570000@gmail.com>
2025-03-04 18:57:32 +0530

    String methods and if and ifesle statement in java.

---
+++ Stringmethods.java
@@ -0,0 +1,24 @@
+public class Stringmethods {
+    public static void main(String[] args) {
+        String name  = (" Vineet Singh");
+        System.out.println(name);
+        int value =name.length();
+        System.out.println(value);
+        String value1 = name.toLowerCase();
+        System.out.println(value1);
+        String value02 =name.toUpperCase();
+        System.out.println(value02);
+        String value03 =name.trim();
+        System.out.println(value03);
+        String value04 =name.substring(3);
+        System.out.println(value04);
+        String value05 =name.substring(2, 2);
+        System.out.println(value05);
+        String value06 =name.replace("Vineet Singh", "8'AWMICDARK");
+        System.out.println(value06);
+        boolean value07 =name.startsWith("Hehehehehe");
+        System.out.println(value07);
+        System.out.println(name.equals(",nvofnvdfnvi"));
+        System.out.println(name.equalsIgnoreCase("dbdicb"));
+    }
+}
---
+++ ifsVineet.java
@@ -0,0 +1,12 @@
+public class ifsVineet {
+    public static void main(String[] args) {
+        int age=(17);
+         if (age>=18) {
+            System.out.println("You can Drive");
+         }
+          else {
+            System.out.println("You can not Drive");
+         }
+        }
+    
+}
