c452bf13e973ddff86ddac83f4df27d8e31125b5
vineet <mrsingh721767570000@gmail.com>
2025-03-04 18:54:02 +0530

    string in java

---
+++ String01.java
@@ -0,0 +1,19 @@
+public class String01 {
+    public static void main(String[] args) {
+       /* /*  String Name = New String("Vineet Singh") */
+       /*  String name =("vineet singh"); */
+      /*   System.out.print("The name is "); */
+        /* System.out.println(name);
+        String age =("20"); */
+        /* System.out.print(" and the age is "); */
+       /*  System.out.println(age); */
+        int A =56;
+        float B =5.39f;
+        System.out.printf("The value of A is %d and the value of B is %f", A,B);
+
+
+
+    }
+      
+    
+}
